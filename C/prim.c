3be2eaa69d58b2b367da0d101d57abca6abd333d
Annihilator <arxeusanion@gmail.com>
2023-03-06 03:02:56 +0530

    omk

--- C/prim.c
+++ C/prim.c
@@ -9,6 +9,45 @@ struct node
 
 struct node Arr[10];
 
+int min(int arr[], int len)
+{
+    int min = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[min] > arr[i])
+        {
+            min = i;
+        }
+    }
+    return min;
+}
+
+void prim(int start, int num)
+{
+    int vis[num];
+    int flag = 0;
+    vis[start] = 1;
+    flag++;
+
+    while (flag <= num)
+    {
+        int num1 = Arr[start].numnei;
+        int arr[num1];
+        for (int i = 0; i < num1; i++)
+        {
+            arr[i] = Arr[start].weight[i]
+        }
+        int min = min(arr,10);
+    }
+}
+
+/*void main()
+{
+    int arr[] = {1,20,40,50,60000,70,30,40,10,234,60};
+    int a = min(arr,10);
+    printf("Min at %d",a+1);
+}*/
+
 void main()
 {
     int num, num1;
@@ -27,17 +66,39 @@ void main()
 
         for (int j = 0; j < num1; j++)
         {
-            printf("Enter neighbour %d \n", j + 1);
+            printf("Enter neighbour number %d - ", j + 1);
             scanf("%d", &Arr[i].nei[j]);
-            printf("Enter the weight of the edge - ");
-            scanf("%d", &Arr[i].weight);
             if (Arr[i].nei[j] > num)
             {
-                printf("Invalid input, try again");
+                printf("Invalid input, try again \n");
                 j--;
+                continue;
             }
+            printf("Enter the weight of the edge - ");
+            scanf("%d", &Arr[i].weight[j]);
         }
+        printf("\n");
     }
+
+    /*for (int i = 0; i < num; i++)
+    {
+        printf("\n");
+        printf("Node %d :- \n", i + 1);
+
+        printf("The neighbours are :- ");
+
+        num1 = Arr[i].numnei;
+        for (int j = 0; j < num1; j++)
+        {
+            printf("%d , ", Arr[i].nei[j]);
+        }
+        printf("The weights are :- ");
+        for (int j = 0; j < num1; j++)
+        {
+            printf("%d , ", Arr[i].weight[j]);
+        }
+        printf("\n");
+    }*/
 
     printf("Enter the node to start from :- ");
     int start;
