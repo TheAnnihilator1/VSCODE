29c7b7e996dfecee76ffca62a4bc69b057d2c500
Annihilator <arxeusanion@gmail.com>
2023-03-06 00:32:39 +0530

    omk

--- C/ballzac.c
+++ C/ballzac.c
@@ -7,7 +7,7 @@ struct cumred
 	float priswei;
 };
 
-void swap(struct cumred* xp, struct cumred* yp)
+void swap(struct cumred *xp, struct cumred *yp)
 {
 	struct cumred temp = *xp;
 	*xp = *yp;
@@ -16,42 +16,42 @@ void swap(struct cumred* xp, struct cumred* yp)
 
 void main()
 {
-	int n,max;
+	int n, max;
 	printf("Enter the number of elements   -  ");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	struct cumred ivan[n];
 	printf("Enter the maximum size  -  ");
-	scanf("%d",&max);
+	scanf("%d", &max);
 	printf("\n");
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
-		printf("Enter price of %d  -  ",i+1);
-		scanf("%f",&ivan[i].pris);
-		printf("Enter the weight of %d - ",i+1);
-		scanf("%f",&ivan[i].wei);
-		ivan[i].priswei = (ivan[i].pris/ivan[i].wei);
+		printf("Enter price of %d  -  ", i + 1);
+		scanf("%f", &ivan[i].pris);
+		printf("Enter the weight of %d - ", i + 1);
+		scanf("%f", &ivan[i].wei);
+		ivan[i].priswei = (ivan[i].pris / ivan[i].wei);
 		printf("\n");
 	}
-	
-	for(int i=0;i<n-1;i++)
+
+	for (int i = 0; i < n - 1; i++)
 	{
-		for(int j=0;j<(n-i-1);j++)
+		for (int j = 0; j < (n - i - 1); j++)
 		{
-			if(ivan[j].priswei > ivan[j + 1].priswei)
+			if (ivan[j].priswei > ivan[j + 1].priswei)
 			{
-				swap(&ivan[j],&ivan[j+1]);
+				swap(&ivan[j], &ivan[j + 1]);
 			}
 		}
 	}
-	
+
 	/*for(int i=0;i<n;i++)
 	{
 		printf("%f %f %f  \n",ivan[i].pris,ivan[i].wei,ivan[i].priswei);
 	}*/
 
-	int i=n-1;
+	int i = n - 1;
 	float answer;
-	while(max>0)
+	while (max > 0)
 	{
 		if (ivan[i].wei < max)
 		{
@@ -65,5 +65,5 @@ void main()
 			max = 0;
 		}
 	}
-	printf("MAX PROFIT is - %f \n" , answer);
+	printf("MAX PROFIT is - %f \n", answer);
 }
