239cc61b3a2443298a45938edf7e9d235039d3c6
Annihilator <arxeusanion@gmail.com>
2023-03-10 07:50:19 +0530

    omk

--- C/kruskal1.c
+++ C/kruskal1.c
@@ -2,6 +2,7 @@
 #define SAX 10
 struct node
 {
+    int ed;
     int v1;
     int v2;
     int weight;
@@ -9,20 +10,61 @@ struct node
 
 struct node Arr[SAX];
 
-void kruskalgal(int start, int num)
+void kruskalgal(int num)
 {
+    for (int step = 0; step < num - 1; ++step)
+    {
+        for (int i = 0; i < num - step - 1; ++i)
+        {
+            if (Arr[i].weight > Arr[i + 1].weight)
+            {
+                struct node temp = Arr[i];
+                Arr[i] = Arr[i + 1];
+                Arr[i + 1] = temp;
+            }
+        }
+    }
+
+    printf("Output \n");
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("Edge %d \n", Arr[i].ed);
+
+        printf("The first vertex %d \n", Arr[i].v1);
+
+        printf("The second vertex %d \n", Arr[i].v2);
+
+        printf("Weight %d \n", Arr[i].weight);
+
+        printf("\n");
+    }
 }
 
 void main()
 {
     int num, num1;
 
-    printf("Enter the number of edge ");
+    printf("Enter the number of edges :- ");
     scanf("%d", &num);
+    printf("\n");
 
     for (int i = 0; i < num; i++)
     {
+        printf("Edge %d \n", i + 1);
+        Arr[i].ed = i + 1;
+
         printf("Enter the first vertex ");
-        scanf("%d", &Arr[i]);
+        scanf("%d", &Arr[i].v1);
+
+        printf("Enter the second vertex ");
+        scanf("%d", &Arr[i].v2);
+
+        printf("Enter the weight ");
+        scanf("%d", &Arr[i].weight);
+
+        printf("\n");
     }
+
+    kruskalgal(num);
 }
